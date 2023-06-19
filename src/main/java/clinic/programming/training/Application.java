ad260e012f6339b2048fdd3672319b1374259744
Abdelhamid Marey <abdelhamid.mahboub@gmail.com>
2023-06-20 00:30:30 +0300

    commit all

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -10,5 +10,12 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	String s = "Hi";
+	int i = switch(s) {
+	case "Hi" -> 5;
+	case "Vy" -> 7;
+	default -> 1;
+	};
+	System.out.println (i);
     }
 }
